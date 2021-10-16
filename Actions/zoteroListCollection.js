ed8c8fd138ce7ae00c52f380df8fc76042c4d688
Kynan Stewart Hughes <mail@kynan.net.au>
2021-10-16 21:55:39 +1100

    ...

--- Actions/zoteroListCollection.js
+++ Actions/zoteroListCollection.js
@@ -2,9 +2,9 @@ const ZOTERO_API_KEY = ___persistence.get("ZOTERO_API_KEY");
 const ZOTERO_API_USER_ID = ___persistence.get("ZOTERO_API_USER_ID");
 
 Zotero.getCollectionItems(ZOTERO_API_KEY, ZOTERO_API_USER_ID)
-    .then((response /*: Object */) /*: Object */ => {
-        return response.getData();
-    })
+    // .then((response /*: Object */) /*: Object */ => {
+    //     return response.getData();
+    // })
     .then((items /*: Object */) /*: Object */ => {
         console.log(items);
     })
--- Libraries/zoteroApiClient.js
+++ Libraries/zoteroApiClient.js
@@ -2,8 +2,12 @@ const Zotero = {
     getCollectionItems: (
         ZOTERO_API_KEY /*: string */,
         ZOTERO_API_USER_ID /*: string */
-    ) /*: void */ => {
+    ) /*: Promise<{zoteroApiKey: string, zoteroApiUserId: string}> */ => {
         console.log("ZOTERO_API_KEY == " + ZOTERO_API_KEY);
         console.log("ZOTERO_API_USER_ID == " + ZOTERO_API_USER_ID);
+        return Promise.resolve({
+            zoteroApiKey: ZOTERO_API_KEY,
+            zoteroApiUserId: ZOTERO_API_USER_ID,
+        });
     },
 };
