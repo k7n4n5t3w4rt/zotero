3c6c51e9f38931f277508e5b4792645f5cd61359
BenkoBot <90161676+BenkoBot@users.noreply.github.com>
2021-10-16 19:20:39 +1100

    File library.js created

---
+++ TrelloListeners/library.js
@@ -0,0 +1,5 @@
+// You can use plain javascript in here,
+// as well as the Trellinator classes for interacting with the Trello API.
+// For more information on Trellinator please consult the docs : http://docs.trellinator.com
+
+const TrelloNotification = new Notification(notification);
