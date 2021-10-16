eaa04c384c455802e6fa242f295140bda5678044
BenkoBot <90161676+BenkoBot@users.noreply.github.com>
2021-10-16 22:06:20 +1100

    File zoteroApiClient.js updated

--- Libraries/zoteroApiClient.js
+++ Libraries/zoteroApiClient.js
@@ -1,13 +1,7 @@
-const Zotero = {
-    getCollectionItems: (
-        ZOTERO_API_KEY /*: string */,
+const Zotero = {getCollectionItems: (
+    ZOTERO_API_KEY /*: string */,
         ZOTERO_API_USER_ID /*: string */
-    ) /*: Promise<{zoteroApiKey: string, zoteroApiUserId: string}> */ => {
-        console.log("ZOTERO_API_KEY == " + ZOTERO_API_KEY);
-        console.log("ZOTERO_API_USER_ID == " + ZOTERO_API_USER_ID);
-        return Promise.resolve({
-            zoteroApiKey: ZOTERO_API_KEY,
-            zoteroApiUserId: ZOTERO_API_USER_ID,
-        });
-    },
-};
+) /*: void */ => {
+    console.log("ZOTERO_API_KEY == " + ZOTERO_API_KEY);
+    console.log("ZOTERO_API_USER_ID == " + ZOTERO_API_USER_ID);
+}};
