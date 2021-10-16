a0bad70faf5520e80f1c3e840cc4e056afd3ec26
Kynan Stewart Hughes <mail@kynan.net.au>
2021-10-16 21:58:47 +1100

    Debug.

--- Actions/zoteroListCollection.js
+++ Actions/zoteroListCollection.js
@@ -1,13 +1,15 @@
 const ZOTERO_API_KEY = ___persistence.get("ZOTERO_API_KEY");
 const ZOTERO_API_USER_ID = ___persistence.get("ZOTERO_API_USER_ID");
 
-Zotero.getCollectionItems(ZOTERO_API_KEY, ZOTERO_API_USER_ID)
-    // .then((response /*: Object */) /*: Object */ => {
-    //     return response.getData();
-    // })
-    .then((items /*: Object */) /*: Object */ => {
-        console.log(items);
-    })
-    .catch((e /*: Error */) /*: void */ => {
-        console.error(e);
-    });
+console.log(Zotero.getCollectionItems);
+
+// Zotero.getCollectionItems(ZOTERO_API_KEY, ZOTERO_API_USER_ID)
+//     // .then((response /*: Object */) /*: Object */ => {
+//     //     return response.getData();
+//     // })
+//     .then((items /*: Object */) /*: Object */ => {
+//         console.log(items);
+//     })
+//     .catch((e /*: Error */) /*: void */ => {
+//         console.error(e);
+//     });
