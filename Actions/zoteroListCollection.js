f5a3e2fdd935485ee539cfe7a3c4947978b8760d
Kynan Stewart Hughes <mail@kynan.net.au>
2021-10-16 22:10:44 +1100

    Passing back a promise.

--- Actions/zoteroListCollection.js
+++ Actions/zoteroListCollection.js
@@ -1,15 +1,16 @@
 const ZOTERO_API_KEY = ___persistence.get("ZOTERO_API_KEY");
 const ZOTERO_API_USER_ID = ___persistence.get("ZOTERO_API_USER_ID");
 
-console.log(Zotero.getCollectionItems(ZOTERO_API_KEY, ZOTERO_API_USER_ID));
+// console.log(Zotero.getCollectionItems(ZOTERO_API_KEY, ZOTERO_API_USER_ID));
 
-// Zotero.getCollectionItems(ZOTERO_API_KEY, ZOTERO_API_USER_ID)
-//     // .then((response /*: Object */) /*: Object */ => {
-//     //     return response.getData();
-//     // })
-//     .then((items /*: Object */) /*: Object */ => {
-//         console.log(items);
-//     })
-//     .catch((e /*: Error */) /*: void */ => {
-//         console.error(e);
-//     });
+Zotero.getCollectionItems(ZOTERO_API_KEY, ZOTERO_API_USER_ID)
+    // .then((response /*: Object */) /*: Object */ => {
+    //     return response.getData();
+    // })
+    .then((items /*: Object */) /*: Object */ => {
+        console.log(items.ZOTERO_API_KEY);
+        console.log(items.ZOTERO_API_USER_ID);
+    })
+    .catch((e /*: Error */) /*: void */ => {
+        console.error(e);
+    });
--- Libraries/zoteroApiClient.js
+++ Libraries/zoteroApiClient.js
@@ -1,7 +1,11 @@
-const Zotero = {getCollectionItems: (
-    ZOTERO_API_KEY /*: string */,
+const Zotero = {
+    getCollectionItems: (
+        ZOTERO_API_KEY /*: string */,
         ZOTERO_API_USER_ID /*: string */
-) /*: void */ => {
-    console.log("ZOTERO_API_KEY == " + ZOTERO_API_KEY);
-    console.log("ZOTERO_API_USER_ID == " + ZOTERO_API_USER_ID);
-}};
+    ) /*: Promise<Object> */ => {
+        return Promise.resolve({
+            ZOTERO_API_KEY,
+            ZOTERO_API_USER_ID,
+        });
+    },
+};
